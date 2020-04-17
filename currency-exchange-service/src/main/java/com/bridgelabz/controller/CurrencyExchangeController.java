7cf5f952907cebf454f5b4106213b950298c9f78
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-04-17 20:49:00 +0530

    [Brijesh] Add. Implemented ExchangeValueRepo JpaRepository to retrieve the data from the DB. And  Modified CurrencyExchangeController class

--- currency-exchange-service/src/main/java/com/bridgelabz/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/bridgelabz/controller/CurrencyExchangeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.ExchangeValue;
+import com.bridgelabz.ExchangeValueRepo;
 
 @RestController()
 public class CurrencyExchangeController {
@@ -16,9 +17,14 @@ public class CurrencyExchangeController {
 	@Autowired
 	private Environment environment;
 	
+	@Autowired
+	private ExchangeValueRepo exchangeValueRepo;
+	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from,@PathVariable String to) {
-		ExchangeValue exchangeValue= new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+//		ExchangeValue exchangeValue= new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+		
+		ExchangeValue exchangeValue= exchangeValueRepo.findByFromAndTo(from, to);
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
 		return exchangeValue;
 	}
