a2bff320ec281d423e29aba05b95a33a687491a7
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-04-18 11:53:30 +0530

    [Brijesh] Add. Implemented Feign mechanism by creating a CurrencyExchangeServiceProxy which makes us easy to talk to RestFull Web Services

--- currency-conversion-service/src/main/java/com/bridgelabz/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/bridgelabz/CurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package com.bridgelabz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.bridgelabz")
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
--- currency-conversion-service/src/main/java/com/bridgelabz/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/bridgelabz/controller/CurrencyConversionController.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,16 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.bridgelabz.CurrencyConversionBean;
+import com.bridgelabz.CurrencyExchangeServiceProxy;
 
 @RestController
 public class CurrencyConversionController {
 
+	@Autowired
+	private CurrencyExchangeServiceProxy proxy;
+	
+	//Feign Problem 1
 	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(@PathVariable String from,
 												  @PathVariable String to,
 												  @PathVariable BigDecimal quantity) 
 	{
-		
+		//Feign Problem 1
 		Map<String,String> uriVariables=new HashMap<>();
 		uriVariables.put("ffrom", from);
 		uriVariables.put("tto", to);
@@ -31,6 +37,17 @@ public class CurrencyConversionController {
 				uriVariables);
 		
 		CurrencyConversionBean response=responseEntity.getBody();
+		
+		return new CurrencyConversionBean(response.getId(), response.getFrom(), response.getTo(),response.getConversionMultiple(), quantity,quantity.multiply(response.getConversionMultiple()), response.getPort());
+	}
+	
+	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from,
+												  @PathVariable String to,
+												  @PathVariable BigDecimal quantity) 
+	{
+		
+		CurrencyConversionBean response=proxy.retrieveExchangeValue(from, to);
 		
 		return new CurrencyConversionBean(response.getId(), response.getFrom(), response.getTo(),response.getConversionMultiple(), quantity,quantity.multiply(response.getConversionMultiple()), response.getPort());
 	}
